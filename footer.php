b60d1c73ecdae40657f083ef90ff809b6e19c9e6
Wu Jiadong <435224787@qq.com>
2014-01-04 14:18:00 +0800

    2.2.2

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 <footer class ="footer">
 	<div class ="pure-menu pure-menu-horizontal pure-menu-open">
 		<ul>
-			<li><a href ="<?php bloginfo('url')?>">Designed by Wu</a></li>
-			<li><a href ="http://purecss.io/">About</a></li>
+			<li><a href ="http://www.wuplus.net">Designed by Wu</a></li>
+			<li><a href ="http://www.wuplus.net">About</a></li>
 			<li><a href ="#">沪ICP备13011252号</a></li>
 		</ul>
     </div>
